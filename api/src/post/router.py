1aee23151833eba3d144aced9cee3c18d49d8038
Gilderlan silva <dacruzgg01@gmail.com>
2025-12-23 12:14:08 -0300

    A Depends() function has been added to all POST endpoints to check who is making the request.

--- api/src/post/router.py
+++ api/src/post/router.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from src.post.schemas import CreatePost
 from src.post.service import PostService
+from src.global_utils.i_request import permitted_origin
 
 router = APIRouter(tags=['CRUD_POST'], prefix='/devorbit')
 
 @router.get('/feed/posts/', summary="Buscar todos os posts")
-async def get_all_posts():
+async def get_all_posts(origin=Depends(permitted_origin)):
     try:
         post_service = PostService()
         posts = await post_service.get_posts_formatted()
@@ -30,17 +31,17 @@ async def get_all_posts():
         )
 
 @router.post('/create/posts/', summary="Criar novo post")
-async def send_posts_in_community(data: CreatePost):
+async def send_posts_in_community(data: CreatePost, origin=Depends(permitted_origin)):
     init_service = PostService()
     var = await init_service.post_create(data=dict(data))
     return var
 
 @router.delete('/delete_posts', summary="Deletar post")
-async def deleted_posts():
+async def deleted_posts(origin=Depends(permitted_origin)):
     init_service = PostService()
     var = await init_service.delete_post(user_id="476347", post_id="768498")
     return var
 
 @router.put('/upgrade_posts', summary="Atualizar post")
-async def upgrade_posts_for_user():
+async def upgrade_posts_for_user(origin=Depends(permitted_origin)):
     pass
